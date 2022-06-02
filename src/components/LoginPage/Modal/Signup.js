60942eef1ff667ca55c9c6a9d7bdd356245c9c34
Jong Min <jmbaek07@gmail.com>
2022-06-02 14:15:06 +0900

    회원가입 error 수정

--- src/components/LoginPage/Modal/Signup.js
+++ src/components/LoginPage/Modal/Signup.js
@@ -2,8 +2,9 @@ import { TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Button } from '@mui/material';
 import { lightGreen } from'@mui/material/colors';
+import { useNavigate } from "react-router-dom";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Modal.css';
 
 
@@ -20,18 +21,23 @@ const LoginButton = styled(Button)({
   
 
 function SignUp(){
-    const [value, setValue] = useState(false);
     const [emailText, setEmailText] = useState('');
     const [nameText, setNameText] = useState('');
     const [passwordText, setPasswordText] = useState('');
 
-    useEffect(()=>{
-        alert('회원가입이 완료되었습니다!');
-        // setValue(false)
-        setEmailText('');
-        setNameText('');
-        setPasswordText('');
-    }, [value])
+    const emailChange = (event)=>{
+        setEmailText(event.target.value);
+    }
+
+    const nameChange = (event)=>{
+        setNameText(event.target.value);
+    }
+
+    const passwordChange = (event)=>{
+        setPasswordText(event.target.value);
+    }
+
+    const navigate = useNavigate();
 
     const submit = async (event) => {
         event.preventDefault();
@@ -40,24 +46,50 @@ function SignUp(){
         const userName =  event.target.name.value;
         const userPassword = event.target.password.value;
 
-        await fetch('http://localhost:3000/app/signup', {
-            method:"POST",
-            headers: {
-                'Content-Type':'application/json; charset=utf-8'
-            },
-            body: JSON.stringify({
-                "email":userEmail,
-                "name":userName,
-                "password":userPassword
-            })
-        })
-        .then((res)=> res.json())
-        .then((res)=>{
-            console.log(res);
-            console.log('test');
-            console.log(res.isSuccess)
-            setValue(res.isSuccess);
-        })
+        async function fetchData(){
+            try {
+                await fetch('http://localhost:8080/app/signup', {
+                    method:"POST",
+                    mode: 'cors',
+                    credentials: 'include',     // 'include', 자격 증명을 포함한 요청 전송
+                    headers: {
+                        'Content-Type':'application/json; charset=utf-8'
+                    },
+                    body: JSON.stringify({
+                        "email":userEmail,
+                        "name":userName,
+                        "password":userPassword
+                    })
+                })
+                .then((res)=> res.json())
+                .then((res)=>{
+                    if(res.isSuccess === true){
+                        return(
+                            // 입력 Text 초기화
+                            setEmailText(''),
+                            setNameText(''),
+                            setPasswordText(''),
+                            
+                            // 페이지 이동
+                            navigate('/'),
+        
+                            // 완료창 띄우기
+                            alert(
+                                `
+                                ${res.result.name} 님 환영합니다.  
+                            회원가입이 완료되었습니다!`)
+                        )
+                    } else if (res.isSuccess === false){
+                        return(
+                            alert(`${res.message}`)
+                        )
+                    }
+                })
+            } catch (error){
+                console.log(error);
+            }
+        }
+        fetchData();
     }
 
     return(
@@ -70,9 +102,9 @@ function SignUp(){
                 </div>
 
                 <form className='Login-item' action="/app/signup" method='post' onSubmit={submit}>
-                    <TextField onChange={emailText} label = "이메일 입력" name = "email" autoComplete='email' margin = "normal" required style ={{width: '85%'}}  autoFocus />
-                    <TextField onChange={nameText} label = "이름 입력" name = "name" margin = "normal" style ={{width: '85%'}} sx={{mt:0.5}} required />
-                    <TextField onChange={passwordText} label = "비밀번호 입력" name = "password" margin = "normal" style ={{width: '85%'}} sx={{mt:0.5}} required />
+                    <TextField value={emailText} onChange={emailChange} label = "이메일 입력" name = "email" autoComplete='email' margin = "normal" required style ={{width: '85%'}}  autoFocus />
+                    <TextField value={nameText} onChange={nameChange} label = "이름 입력" name = "name" margin = "normal" style ={{width: '85%'}} sx={{mt:0.5}} required />
+                    <TextField value={passwordText} onChange={passwordChange} label = "비밀번호 입력" name = "password" margin = "normal" style ={{width: '85%'}} sx={{mt:0.5}} required />
                     <LoginButton type="submit" variant="contained">회원가입</LoginButton>
                 </form>
 
